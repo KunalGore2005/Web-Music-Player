a5438733dc2fb3cef368c6f9311bebfd12ce0d73
Kunal Gore <gorekunal665@gmail.com>
2026-06-30 23:59:33 +0530

    Updated after deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes');
 const musicRoutes = require('./routes/music.routes');
@@ -6,18 +7,10 @@ const musicRoutes = require('./routes/music.routes');
 
 const app = express();
 
-// Custom CORS middleware to allow credentials (cookies) cross-origin
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  res.setHeader('Access-Control-Allow-Origin', origin || '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
